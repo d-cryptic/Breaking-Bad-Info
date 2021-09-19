b9e4c508e30ba836b7e242adb542ac82706f536c
Barun Debnath <barundebnath91@gmail.com>
2021-09-19 23:27:10 +0530

    css and html done

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const inpSearch = document.getElementById("inp-search");
+const output = document.getElementById("output");
+
+window.addEventListener("load", () => {
+  fetchCharacters();
+});
+
+async function fetchCharacters(query) {
+  let;
+}
